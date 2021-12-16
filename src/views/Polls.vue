eeff70ec090829ded23e0c6cf1aeac2e41556222
Melissa Tro <s0571267@htw-berlin.de>
2021-12-16 11:52:20 +0100

    Add static person layout

--- src/views/Polls.vue
+++ src/views/Polls.vue
@@ -1,12 +1,38 @@
-<template>
-  <h1>Your Polls</h1>
-</template>
-
-<script>
-export default {
-  name: 'Polls'
-}
-</script>
-
-<style scoped>
-</style>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+<div class="col">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Poll</h5>
+      <p class="card-text">This is a poll you created on 12.12.2021.</p>
+    </div>
+  </div>
+</div>
+<div class="col">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Poll 2</h5>
+      <p class="card-text">This is a poll you created on 16.12.2021.</p>
+    </div>
+  </div>
+</div>
+<div class="col">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+</div>
+<div class="col">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+</div>
+</div>
