2634e9a160897b481e9d7b7cf7869c56d5ea20c8
Melissa Tro <s0571267@htw-berlin.de>
2021-12-16 11:04:54 +0100

    add yml file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <navbar></navbar>
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/Navbar'
+export default {
+  name: 'App',
+  components: { Navbar }
+}
+</script>
 
 <style>
 #app {
---
+++ src/views/Polls.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Your Polls</h1>
+</template>
+
+<script>
+export default {
+  name: 'Polls'
+}
+</script>
+
+<style scoped>
+</style>
