51ed4974750d00b98230ed50a3ffcae1eca80b11
Melissa Tro <s0571267@htw-berlin.de>
2021-12-17 13:05:11 +0100

    Fix frontend polls.vue

--- src/views/Polls.vue
+++ src/views/Polls.vue
@@ -1,38 +1,57 @@
-<div class="row row-cols-1 row-cols-md-2 g-4">
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Poll</h5>
-      <p class="card-text">This is a poll you created on 12.12.2021.</p>
+<template>
+  <h1>Welcome to your Polls</h1>
+  <div class="container-fluid">
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+   <div class="col" v-for="poll in polls" :key="poll.id">
+      <div class="card h-100">
+        <div class="card-body">
+          <img src="https://www.voxco.com/wp-content/uploads/2021/09/Opinion-Polls1.png" class="card-img-top" alt="...">
+          <h5 class="card-title">{{ poll.title}} {{ poll.name }}</h5>
+          <p class="card-text">
+            Der Poll "{{ poll.title }}" von {{ poll.username }} wurde am {{ poll.creationDate }} erstellt und
+            hat {{ poll.options.length }} Optionen.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Poll 2</h5>
-      <p class="card-text">This is a poll you created on 16.12.2021.</p>
-    </div>
   </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-</div>
-</div>
+</template>
+
+<script>
+export default {
+  name: 'Polls',
+  data () {
+    return {
+      polls: [
+        {
+          id: 1,
+          title: 'Wann könnt ihr?',
+          username: 'timtomthomas',
+          creationDate: '12.12.2021',
+          endDate: '18.12.2021',
+          options: [
+            '16.12.21',
+            '17.12.21',
+            '18.12.21'
+          ]
+        },
+        {
+          id: 1,
+          title: 'Wo treffen wir uns?',
+          username: 'timtomthomas',
+          creationDate: '12.12.2021',
+          endDate: '16.12.2021',
+          options: [
+            'draußen',
+            'zuhause'
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
