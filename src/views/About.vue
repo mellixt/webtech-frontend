600ff467451a325f9a5559a4daab9b2ed09e61d0
Melissa Tro <s0571267@htw-berlin.de>
2022-01-02 17:42:39 +0100

    Add About Us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div style="width: 100%; float:left">
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51cOM2ZPaoL.png" style="width:13%" alt="...">
+      <br>
+      <br>
+      <p style="color:#F1F1F1 ; font-size:80%;" ><b>Image from:</b> https://images-na.ssl-images-amazon.com/images/I/51cOM2ZPaoL.png</p>
+    </div>
+    <div style="width: 100%; float:right">
+      <textbox>
+        <br>
+        <br>
+        <h1><b>About Us</b></h1><br>
+        <b>Need a quick way to plan your next meet-up?<br>
+          Maybe you want to decide the next location to hang out at?</b><br>
+          <br>
+        <router-link to="/polls" tag="button" class="btn btn-outline-success"><b>  GET STARTED NOW </b> </router-link>
+        <br>
+        <br>
+        <br>
+        <span style="color:indianred">
+        This website is for everyone who wants to make a decision as<br>
+        quick and easy as possible. Just let the majority decide.<br>
+        You can create a simple poll and share the link with<br>
+        your friends or collegues in question.<br>
+        </span>
+      </textbox>
+    </div>
   </div>
 </template>
