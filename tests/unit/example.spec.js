661a82c93f80fec32feffa041aa448cc60df5ed5
Melissa Tro <s0571267@htw-berlin.de>
2022-01-09 21:08:49 +0100

    - adding test for navbar, removing old test for helloworld

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,22 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+import Polls from '@/views/Polls.vue'
+import Vue from 'vue'
 
-describe('HelloWorld.vue', () => {
+describe('NavBar.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'My Poll AppNew PollAbout'
+    const wrapper = shallowMount(NavBar, {
       props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
+  })
+})
+describe('Polls.vue', () => {
+  it('checks if polls exist', () => {
+    // build component
+    const Constructor = Vue.extend(Polls)
+    const PollsComponent = new Constructor().$mount()
+    expect(PollsComponent)
   })
 })
